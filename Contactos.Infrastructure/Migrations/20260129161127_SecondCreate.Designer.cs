978e8efb0a6bc7a49ef47bf529d04845d7364a52
Carlou134 <carlouvasquez134@gmail.com>
2026-01-29 11:12:34 -0500

    Migrations

--- Contactos.Infrastructure/Migrations/20260129161127_SecondCreate.Designer.cs
+++ Contactos.Infrastructure/Migrations/20260129161127_SecondCreate.Designer.cs
@@ -3,6 +3,7 @@ using Contactos.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Contactos.Infrastructure.Migrations
 {
     [DbContext(typeof(ContactoContext))]
-    partial class ContactoContextModelSnapshot : ModelSnapshot
+    [Migration("20260129161127_SecondCreate")]
+    partial class SecondCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Contactos.Infrastructure/Migrations/20260129161127_SecondCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Contactos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Teléfono",
+                table: "Contactos",
+                newName: "Telefono");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Telefono",
+                table: "Contactos",
+                newName: "Teléfono");
+        }
+    }
+}
