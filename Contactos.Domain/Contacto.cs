7f4b0a21ff47c8068ace0e0e545746fce425458c
Carlou134 <carlouvasquez134@gmail.com>
2026-01-29 11:03:56 -0500

    Cambio

--- Contactos.Domain/Contacto.cs
+++ Contactos.Domain/Contacto.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Nombre { get; set; } = string.Empty;
         public string Email {  get; set; } = string.Empty;
-        public string Teléfono { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
     }
 }
